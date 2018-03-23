b9ead2b225ffd723f990a0b445708fa631ca9713
Hersh Krishna <HershKrishna@utexas.edu>
2018-03-22 20:39:32 -0500

    Added short typedefs

--- def.h
+++ def.h
@@ -9,6 +9,16 @@
 #define DEBUG 1
 #endif
 
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
 /* Types and constants */
 
 #define local static /* Function is local to the file */

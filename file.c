cb16001f4f54b2d246c2e905d520168e4feb10fb
Hersh Krishna <HershKrishna@utexas.edu>
2018-04-02 19:32:42 -0500

    Added file.c MapFileToRoBuffer

--- file.c
+++ file.c
@@ -3,10 +3,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* File related includes */
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <sys/mman.h>
+
 char *BaseName(char *restrict destBuf, const char *restrict pathstr)
 {
     /* TODO: This function is doing a lot of unnecessary work. Do this simpler? */
     strcpy(destBuf, pathstr);
     return basename(destBuf);
 
+}
+
+char *MapFileToROBuffer(const char *filename, void *addrHint, size_t *fileLength)
+{
+    int fd = open(filename, O_RDONLY);
+
+    if (fd == -1)
+    {
+	return NULL;
+    }
+
+    struct stat st = {};
+
+    if (fstat(fd, &st) == -1)
+    {
+	return NULL;
+    }
+
+    size_t fileSize = st.st_size;
+
+    char *fileBuf = mmap(addrHint, fileSize, PROT_READ, MAP_PRIVATE, fd, 0);
+
+    if(fileLength)
+    {
+	*fileLength = fileSize;
+    }
+
+    close(fd);
+
+    return fileBuf;
 }
--- file.h
+++ file.h
@@ -3,4 +3,5 @@
 
 char *BaseName(char *restrict destBuf, const char *restrict pathstr);
 
+char *MapFileToROBuffer(const char *filename, void *addrHint, size_t *fileLength);
 #endif

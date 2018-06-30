6ebd58d9d340ea630c6c304d072b52723b6a7fb5
Hersh Krishna <HershKrishna@utexas.edu>
2018-06-30 09:59:44 -0500

    Removed strcpyi name that was still left

--- string.h
+++ string.h
@@ -7,9 +7,6 @@ bool StrStartsWith(char *string, char *subString, size_t subStringLen);
 
 char **SplitLines(char *req);
 
-/* Needs a better name */
-int strcpyi(char *restrict dest, const char *restrict src, size_t maxdeststrlen);
-
 bool streq(char *str1, char *str2);
 bool strneq(char *str1, char *str2, size_t n);
 

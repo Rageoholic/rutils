92947b253fdca14eb6ce825a98528ef69c8af2dc
Hersh Krishna <HershKrishna@utexas.edu>
2018-09-21 21:21:23 -0500

    Added a bunch of integer types

--- def.h
+++ def.h
@@ -3,7 +3,8 @@
 
 #include <stdbool.h>
 #include <stddef.h>
-#include <stdint.h>
+#include <inttypes.h>
+
 
 #ifndef NDEBUG
 #define DEBUG 1
@@ -31,10 +32,37 @@ typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
+typedef uint_least8_t ul8;
+typedef uint_least16_t ul16;
+typedef uint_least32_t ul32;
+typedef uint_least64_t ul64;
+
+typedef uint_fast8_t uf8;
+typedef uint_fast16_t uf16;
+typedef uint_fast32_t uf32;
+typedef uint_fast64_t uf64;
+
+/* I don't really end up using these. They're here for legacy */
 typedef int8_t s8;
 typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+
+typedef int_least8_t il8;
+typedef int_least16_t il16;
+typedef int_least32_t il32;
+typedef int_least64_t il64;
+
+typedef int_fast8_t if8;
+typedef int_fast16_t if16;
+typedef int_fast32_t if32;
+typedef int_fast64_t if64;
 
 typedef unsigned char byte;
 

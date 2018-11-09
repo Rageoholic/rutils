dc3a0373d67faa3ba426e596376a44b0bb4822fb
Hersh Krishna <hershkrishna@protonmail.ch>
2018-11-08 19:12:37 -0600

    Added INVARIANT macro

--- debug.h
+++ debug.h
@@ -7,11 +7,21 @@
 #ifdef NDEBUG
 #define ERROR(str)
 #else
-#define ERROR(str)                                                            \
-    do                                                                        \
-    {                                                                         \
-        fprintf(stderr, "fatal error at %s:%d: %s", __FILE__, __LINE__, str); \
-        exit(-1);                                                             \
+#define ERROR(str)                                                              \
+    do                                                                          \
+    {                                                                           \
+        fprintf(stderr, "fatal error at %s:%d: %s\n", __FILE__, __LINE__, str); \
+        abort();                                                                \
     } while (0);
 #endif
+
+#define INVARIANT(expr, str)                                                               \
+    do                                                                                     \
+    {                                                                                      \
+        if (!(expr))                                                                       \
+        {                                                                                  \
+            fprintf(stderr, "Invariant violated at %s:%d: %s\n", __FILE__, __LINE__, str); \
+            abort();                                                                       \
+        }                                                                                  \
+    } while (0)
 #endif

7ccb524621cf8d72a3d55e95ea5296e0fe5f06d6
Hersh Krishna <hershkrishna@protonmail.ch>
2018-11-19 18:14:15 -0600

    Switched ERROR to always be on even in prod builds

--- debug.h
+++ debug.h
@@ -3,17 +3,13 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-/* Functions */
-#ifdef NDEBUG
-#define ERROR(str)
-#else
+
 #define ERROR(str)                                                              \
     do                                                                          \
     {                                                                           \
         fprintf(stderr, "fatal error at %s:%d: %s\n", __FILE__, __LINE__, str); \
         abort();                                                                \
     } while (0);
-#endif
 
 #define INVARIANT(expr, str)                                                               \
     do                                                                                     \
